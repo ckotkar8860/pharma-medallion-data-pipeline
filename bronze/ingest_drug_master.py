9556c3d721ae70a4468a697e15230e1bfa6f298d
ckotkar8860 <chirayukotkar99@gmail.com>
2026-02-03 11:51:56 +0530

    Update ingest_drug_master.py

--- bronze/ingest_drug_master.py
+++ bronze/ingest_drug_master.py
@@ -1,9 +1,10 @@
-from pyspark.sql import SparkSession
+from utils.spark_session import get_spark_session
+from utils.constants import RAW_PATH, BRONZE_PATH
 
-spark = SparkSession.builder.appName("bronze_patients").getOrCreate()
+spark = get_spark_session("bronze_drug_master")
 
-df = spark.read.option("header", True).csv("/mnt/data/raw/patients.csv")
+df = spark.read.option("header", True).csv(f"{RAW_PATH}/drug_master.csv")
 
 df.write.format("delta") \
-  .mode("append") \
-  .save("/mnt/delta/bronze/patients")
+    .mode("append") \
+    .save(f"{BRONZE_PATH}/drug_master")
